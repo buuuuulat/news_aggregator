1d3c3a2ac5aff84c3d5b1f3bb4bb1e327e33153a
buuuuulat <156512428+buuuuulat@users.noreply.github.com>
2025-10-03 23:13:11 +0300

    Large texts are now being chunked

--- summarizer/model.py
+++ summarizer/model.py
@@ -16,36 +16,50 @@ model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME)
 model.to(device)
 model.eval()
 
-def summarize(text: str, max_new_tokens: int = 120) -> str:
+def chunk_text(text: str):
+    chunks = []
+    if len(text) > 1000:
+        for i in range(int(len(text)/1000) + 1, len(text), 1000):
+            chunks.append(text[i:i+1000])
+    else:
+        chunks.append(text)
+    return chunks
+
+def summarize(text: str, max_new_tokens: int = 120) -> list[str]:
     """
     Краткое резюме текста на 2–3 предложения.
     Модель — ruT5.
     """
-    prompt = f"summarize: {text}\nКратко, 2–3 предложения."
-    inputs = tokenizer(
-        prompt,
-        return_tensors="pt",
-        truncation=True,
-        max_length=1024, # Срезаем слишком длинные входы
-    ).to(device)
+    chunks = chunk_text(text)
+    summaries = []
+    for chunk in chunks:
+        prompt = f"summarize: {text}\nКратко, 2–3 предложения."
+        inputs = tokenizer(
+            prompt,
+            return_tensors="pt",
+            truncation=True,
+            max_length=1024, # Срезаем слишком длинные входы
+        ).to(device)
 
-    with torch.no_grad():
-        output_ids = model.generate(
-            **inputs,
-            do_sample=False,
-            num_beams=4,
-            length_penalty=0.8,
-            max_new_tokens=max_new_tokens,
-            no_repeat_ngram_size=3,
-            eos_token_id=tokenizer.eos_token_id,
-        )
+        with torch.no_grad():
+            output_ids = model.generate(
+                **inputs,
+                do_sample=False,
+                num_beams=4,
+                length_penalty=0.8,
+                max_new_tokens=max_new_tokens,
+                no_repeat_ngram_size=3,
+                eos_token_id=tokenizer.eos_token_id,
+            )
 
-    summary = tokenizer.decode(output_ids[0], skip_special_tokens=True).strip()
+        summary = tokenizer.decode(output_ids[0], skip_special_tokens=True).strip()
 
-    # Нормализуем до 2–3 предложений на всякий случай
-    sentences = re.split(r'(?<=[.!?])\s+', summary)
-    summary_3 = " ".join(sentences[:3]).strip()
-    return summary_3
+        # Нормализуем до 2–3 предложений на всякий случай
+        sentences = re.split(r'(?<=[.!?])\s+', summary)
+        summary_3 = " ".join(sentences[:3]).strip()
+
+        summaries.append(summary_3)
+    return summaries
 
 # Input prompt here
 article = """
@@ -60,6 +74,5 @@ article = """
 
 
 if __name__ == "__main__":
-    print("Device:", device)
-    print("\nКраткий пересказ:\n")
     print(summarize(article))
+
