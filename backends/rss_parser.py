7f1c2908b1d2bfc527ea3b195966acfc9c209856
buuuuulat <156512428+buuuuulat@users.noreply.github.com>
2025-10-04 18:27:32 +0300

    rss_parser.py added

--- backends/rss_parser.py
+++ backends/rss_parser.py
@@ -1,4 +1,21 @@
 import feedparser
 
-feed = feedparser.parse("https://lenta.ru/rss/news")
-print(feed)
+
+def parse_websites(websites: list[str], keywords: list[str], latest_n: int=10):
+    """
+    :param websites: List of RSS links to the news websites
+    :param keywords: Keywords filters (soon)
+    :param latest_n: Choose last n articles from each source (-1 for all)
+    :return: Dict: {rss_source_link: [dicts of RSS parsed params]}
+    """
+    parsed = {}
+    for website in websites:
+        parsed[website] = feedparser.parse(website)["entries"][:latest_n]
+    return parsed
+
+def parse_tg(tg_links: list[str], keywords: list[str], latest_n: int=10):
+    pass
+
+print(parse_websites(["http://lenta.ru/rss/news", "https://ria.ru/export/rss2/archive/index.xml"],
+                     ["nigga"], 1))
+
